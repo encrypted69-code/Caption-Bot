36250d2b4bfb73c8e13eb1db7fcc9664b86afa63
encrypted69-code <iamgreedy6@gmail.com>
2025-09-07 23:25:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-import os
-
-
-
 class Config(object):
-      BOT_TOKEN = os.environ.get("8400875138:AAE2zrn4ovvwAj98NPbuXALsPm3rSiRtRIk", "")
-      API_ID = int(os.environ.get("25439876"))
-      API_HASH = os.environ.get("ae454c231329a1c7e3af30f552a29dc2")
-      CAPTION_TEXT = os.environ.get("hello this is encrypted", "")
-      CAPTION_POSITION = os.environ.get("BOTTOM", "nil")
-      ADMIN_USERNAME = os.environ.get("encryptedxyz", "AvishkarPatil")
+    BOT_TOKEN = "8400875138:AAE2zrn4ovvwAj98NPbuXALsPm3rSiRtRIk"
+    API_ID = 25439876
+    API_HASH = "ae454c231329a1c7e3af30f552a29dc2"
+    CAPTION_TEXT = "hello this is encrypted"
+    CAPTION_POSITION = "BOTTOM"
+    ADMIN_USERNAME = "encryptedxyz"
